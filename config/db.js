3b54d33da52375a9e8705df3f32f9f705359d204
koftov <ahronlu@gmail.com>
2020-10-19 13:02:33 +0300

    dotenv and PWA

--- config/db.js
+++ config/db.js
@@ -1,17 +1,15 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(process.env.mongoURI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
     });
 
-    console.log('MongoDB Connected...');
+    console.log("MongoDB Connected...");
   } catch (err) {
     console.error(err.message);
     process.exit(1);

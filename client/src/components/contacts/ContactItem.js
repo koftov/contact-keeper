8b902a70949fb84e5deec4a3996be651112f71e1
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-05-30 00:02:33 +0300

    Update ContactItem.js

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -30,17 +30,17 @@ const ContactItem = ({ contact }) => {
       <ul className='list'>
           {address && (
           <li>
-            <i className='fas fa-map-marker-alt' /> <a href=`https://maps.google.com/?q=${address}`>{address}</a>
+            <i className='fas fa-map-marker-alt' /> <a href={`https://maps.google.com/?q=${address}`}>{address}</a>
           </li>
         )}
         {email && (
           <li>
-            <i className='fas fa-envelope-open' /> <a href=`mailto:${email}`>{email}</a>
+            <i className='fas fa-envelope-open' /> <a href={`mailto:${email}`}>{email}</a>
           </li>
         )}
         {phone && (
           <li>
-            <i className='fas fa-phone' /> <a hef=`tel:${phone}`>{phone}</a>
+            <i className='fas fa-phone' /> <a hef={`tel:${phone}`}>{phone}</a>
           </li>
         )}
       </ul>
